14381ffd53220f3873d8c608153a5ba367c5ccb6
DESKTOP-JOOKDG6\profesor <csi23-amoliaz@colegioaltair.net>
2023-11-20 13:34:49 +0100

    Agregar archivos de proyecto.

---
+++ Controladores/Program.cs
@@ -0,0 +1,4 @@
+﻿//dar alta una nueva cuenta bancaria ,
+//tiene que terner un campo que identifique al usuario de esa cuenta bancaria
+//id, isban, codigo switch del banco, a que cliente le pertenece esa cuenta bancaria , fecha alta y baja de la cuenta
+us
---
+++ Servicios/menuI.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClienteYBancoC_.Servicios
+{
+    /// <summary>
+    /// interfaz encragada de mostra un menu y guarda la eleccion del usuario
+    /// amd-201123
+    /// </summary>
+    internal interface menuI
+    {
+        /// <summary>
+        /// metodo que mostra un menu y guarda la eleccion del usuario
+        /// amd-201123
+        /// </summary>
+        /// <returns>int</returns>
+        public int menuCl();
+    }
+}
